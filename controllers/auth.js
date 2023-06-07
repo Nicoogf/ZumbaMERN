166485bcd916ccd3969c9434183453e2746148b1
Nicoogf <nicolasgfalabella@gmail.com>
2023-06-07 18:22:51 -0300

    Se trabaja sobre el Auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,3 +1,30 @@
 import bcrypt from "bcrypt" ;
 import jwt from "jsonwebtoken" ;
 import User from "../models/User.js" ;
+
+/*Registro de Usuarios*/
+
+ export const register = async ( req , res) =>{
+    try{
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picture,
+            friend,
+            location,
+            occupation,
+        } = req.body ;
+
+
+        const  salt = await bcrypt.genSalt();   //Encriptar contraseña
+        const passwordHash =  await bcrypt.hash( password, salt )
+
+        const newUser = new User({
+            
+        }) 
+    }catch(err){
+
+    }
+ }
